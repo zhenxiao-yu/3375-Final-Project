6f909108cf8df312b2c45084e413a4a037cb7116
zhenxiao-yu <markyu0615@gmail.com>
2022-04-11 22:45:53 -0400

    REMAKE

--- Calculator.c
+++ Calculator.c
@@ -1,7 +1,6 @@
 #include "address_map_arm.h";
 
 // define mappings manually for simulator
-#define MPCORE_PRIV_TIMER 0xFFFEC600
 #define HEX3_HEX0_BASE 0xFF200020
 #define HEX5_HEX4_BASE 0xFF200030
 #define SW_BASE 0xFF200040
@@ -9,6 +8,52 @@
 
 volatile int *Display1_ptr = (int *)HEX3_HEX0_BASE;
 volatile int *Display2_ptr = (int *)HEX5_HEX4_BASE;
+
+//* utilty functions*//
+int countDigit (int val) {
+    int count = 0;
+    while (val!= 0) {
+        val = val/10;
+        count++;
+    }
+    return count;
+}
+
+//* display message functions*//
+// Display 'Error' Message on 7-segment display
+void DisplayError() {
+    int BYTE = 8;
+    int Letter_E = 0x79; //1111001
+    int letter_r = 0x50; //1010000
+    int letter_o = 0x5C; //1011100
+    // Displaying r + o
+    *Display1_ptr = letter_r << (3 * BYTE) |
+                    letter_r << (2 * BYTE) |
+                    letter_o << (1 * BYTE) |
+                    letter_r << (0 * BYTE);
+     // Displaying E
+    *Display2_ptr = Letter_E;
+}
+
+void DisplayOperator(int oprID) {
+    char oprTable[5];
+    oprTable[0] = 0x3F; // used as capital letter 'O'
+    oprTable[1] = 0x06; // + 
+    oprTable[2] = 0x5B; // -
+    oprTable[3] = 0x4F; // x
+    oprTable[4] = 0x66; // /
+
+    int BYTE = 8;
+    int letter_p = 0x73; //1110011
+    int letter_r = 0x50; //1010000
+    // Displaying O + p + r + operator ID
+    *Display1_ptr = oprTable[0]     << (3 * BYTE) |
+                    letter_p        << (2 * BYTE) |
+                    letter_r        << (1 * BYTE) |
+                    oprTable[oprID] << (0 * BYTE);
+    // turn off
+    *Display2_ptr = 0x00;
+}
 
 // Displays number on 7 seg display
 void DisplayValue(int value)
@@ -26,16 +71,53 @@ void DisplayValue(int value)
     lookUpTable[9] = 0x6F;
 
     int BYTE = 8;
-    int bit1 = lookUpTable[1];
-    int bit2 = lookUpTable[1];
-    int bit3 = lookUpTable[1]; 
-    int bit4 = lookUpTable[1]; 
-    int bit5 = lookUpTable[1];
-    int bit6 = lookUpTable[1];
-
+    int bit1, bit2, bit3, bit4, bit5, bit6;
     // if value can be represented by 6 digits
-    if (countDigit(value) <= 6)
+     int bitArray[6];
+     int i = 5;
+     while (i >= 0) {
+         bitArray[i] = value%10;
+         value/=10;
+         i--;
+     }
+     if (bitArray[0] != 0) {
+         bit6 =  lookUpTable[bitArray[0]];
+     } else {
+         bit6 = 0x00;
+     }
+     if (bitArray[0] != 0 && bitArray[1] != 0 ) {
+         bit5 =  lookUpTable[bitArray[1]];
+     } else {
+         bit6 = 0x00;
+         bit5 = 0x00;
+     }
+     if (bitArray[0] != 0 && bitArray[1] != 0 && bitArray[2] != 0 ) {
+         bit4 =  lookUpTable[bitArray[2]]; 
+     } else {
+         bit6 = 0x00;
+         bit5 = 0x00;
+         bit4 = 0x00;
+     }
+     if (bitArray[0] != 0 && bitArray[1] != 0 && bitArray[2] != 0 && bitArray[3] != 0) {
+         bit3 =  lookUpTable[bitArray[3]];
+     } else {
+         bit6 = 0x00;
+         bit5 = 0x00;
+         bit4 = 0x00;  
+         bit3 = 0x00; 
+     } 
+     if (bitArray[0] != 0 && bitArray[1] != 0 && bitArray[2] != 0 && bitArray[3] != 0 &&  bitArray[4] != 0) {
+         bit2 =  lookUpTable[bitArray[4]];
+     } else {
+         bit6 = 0x00;
+         bit5 = 0x00;
+         bit4 = 0x00;  
+         bit3 = 0x00; 
+         bit2 = 0x00; 
+     }
+     bit1 =  lookUpTable[bitArray[5]];
 
+    
     // Displaying bit 1 - 4 of value 
     *Display1_ptr = bit4 << (3 * BYTE) |
                     bit3 << (2 * BYTE) |
@@ -46,31 +128,6 @@ void DisplayValue(int value)
      // Displaying bit 5 - 6 of value 
     *Display2_ptr = bit6 << (1 * BYTE) |
                     bit5 << (0 * BYTE);
-}
-
-//* utilty functions*//
-int countDigit (int val) {
-    int count = 0;
-    while (val!= 0) {
-        val = val/10;
-        count++;
-    }
-    return count;
-}
-
-// Display 'Error' Message on 7-segment display
-void DisplayError() {
-    int BYTE = 8;
-    int Letter_E = 0x79;
-    int letter_r = 0x50;
-    int letter_o = 0x5C;
-    // Displaying r + o
-    *Display1_ptr = letter_r << (3 * BYTE) |
-                    letter_r << (2 * BYTE) |
-                    letter_o << (1 * BYTE) |
-                    letter_r << (0 * BYTE);
-     // Displaying E
-    *Display2_ptr = Letter_E;
 }
 
 // Reads switches
@@ -88,33 +145,49 @@ int ReadButton(int btn)
     return ((*BTN_ptr >> (btn)) & 1);
 }
 
+// get input from switch 0 - 5
+int GetInput(void)
+{
+    // pointer to switch address
+    volatile int *SW_ptr = (int *)SW_BASE;
+    int inputValue = *(SW_ptr);
+    // reads slide switch bank and return current value (encoded in binary)
+    // return lowest 6 bits in the switch bank (2 ^ 6)
+    return (inputValue % (2 * 2 * 2 * 2 * 2 * 2));
+}
+
+
 int main(void)
 {
-    // Sets initial display to 0
-    DisplayValue(0);
+
 
     // Main loop
     while (1)
     {   
+        if (countDigit(GetInput()) <= 6) {
+            DisplayValue(GetInput());
+        } else {
+        DisplayError();
+        }
         // plus
-        if (ReadButton(0))
+        if (ReadButton(0) && !ReadButton(1) && !ReadButton(3) && !ReadButton(3))
         {
-            DisplayError();
+           DisplayOperator(1);
         }
         // minus
-        if (ReadButton(1))
+        if (!ReadButton(0) && ReadButton(1) && !ReadButton(2) && !ReadButton(3))
         {
-
+            DisplayOperator(2);
         }
         // multiply
-        if (ReadButton(2))
+        if (!ReadButton(0) && !ReadButton(1) && ReadButton(2) && !ReadButton(3))
         {
-
+            DisplayOperator(3);
         }
         //divide
-        if (ReadButton(3))
+        if (!ReadButton(0) && !ReadButton(1) && !ReadButton(2) && ReadButton(3))
         {
-
+            DisplayOperator(4);
         }
 
 
